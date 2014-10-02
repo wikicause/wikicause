ab0fcca1b45a724f59515c93cf03c1efbe58e75e
Trevor Starick <trevor.starick@gmail.com>
2014-10-02 04:01:36 -0400

    HBS instead of Static pages

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var logger = require('morgan');
+var hbs = require('express-hbs');
 
 // var routes = require('./routes');
 var app = express();
 
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express3({
+  partialsDir: __dirname + '/views/partials',
+  defaultLayout: __dirname + '/views/layout.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req,res) {
+	res.render('index');
+});
+
+app.get('/new', function(req,res) {
+	res.render('item');
+});
+
+app.get('/edit', function(req,res) {
+	res.render('item');
+});
+
 // app.use('*', routes);
 
 var PORT = process.env.PORT || 8080;
