341763988dc08ab2388f568bb979944d8df6e9e9
Trevor Starick <trevor.starick@gmail.com>
2014-10-10 13:37:00 -0400

    New and improved dataset generator

--- gen-dataset.js
+++ gen-dataset.js
@@ -4,27 +4,27 @@ var nodes = [
   ['ff', 'ff', '21', '22', '23', '24']
 ];
 
-var data = {
-  _id: id,
-  slug: slugify(content.title),
-  parents: parent,
-  children: [],
-  data: {
-    title: content.title,
-    subtitle: content.subtitle,
-    img: content.img,
-    href: content.href,
-    contributors: [{
-      name: 'Trevor Starick',
-      twitter: '@trevorstarick',
-      email: 'trevor.starick@gmail.com'
-    }]
-  },
-  event: {
-    id: event.id,
-    slug: event.slug
-  }
-};
+// var data = {
+//   _id: id,
+//   slug: slugify(content.title),
+//   parents: parent,
+//   children: [],
+//   data: {
+//     title: content.title,
+//     subtitle: content.subtitle,
+//     img: content.img,
+//     href: content.href,
+//     contributors: [{
+//       name: 'Trevor Starick',
+//       twitter: '@trevorstarick',
+//       email: 'trevor.starick@gmail.com'
+//     }]
+//   },
+//   event: {
+//     id: event.id,
+//     slug: event.slug
+//   }
+// };
 
 nodes.forEach(function(r,i) {
   r.forEach(function(c) {
